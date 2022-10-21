1eca29c350a6019e6c8ce73a34ba5e625cd8cfb9
oxxd <oxxxxd@gmail.com>
2022-10-22 01:47:01 +0800

    chore: 项目初始化配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const plugins = [];
+
+// 打包生产环境才引入的插件
+if (process.env.NODE_ENV === 'production') {
+  process.env.ANALYZE &&
+    plugins.push(
+      visualizer({
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+      })
+    );
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [splitVendorChunkPlugin(), react(), ...plugins],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: '/src',
+      },
+    ],
+  },
+});
